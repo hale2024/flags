6d7c1289e7780ddcb41f6013802994a17fe28f2a
Pratik Devkota <reusedbottle@proton.me>
2022-06-25 22:11:04 +0545

    Update pwn.college/embryoio/embryoio_level111/handle_signals.py

--- pwn.college/embryoio/embryoio_level111/handle_signals.py
+++ pwn.college/embryoio/embryoio_level111/handle_signals.py
@@ -5,7 +5,7 @@ import subprocess
 
 pid = int( input("Enter PID of the other process: ") );
 signals_str = str( input("Enter list of signals it gave: ") );
-signals = list(ast.literal_eval(signals_str));
+signals = list( ast.literal_eval(signals_str) );
 
 for sig in signals:
     sig_num = signal.Signals[sig].value;
