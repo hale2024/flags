ef4d0bc694d93d1524171fbaf2ba245e272fe36b
Pratik Devkota <reusedbottle@proton.me>
2022-07-11 08:10:53 +0545

    Add pwn.college/embryoasm/{1,2,3,4,5}.py

---
+++ pwn.college/embryoasm/5.py
@@ -0,0 +1,21 @@
+from pwn import *
+
+# set some context
+context.arch = "amd64"
+context.encoding = "latin"
+# ignore warnings
+warnings.simplefilter("ignore")
+
+# assembly goes here, most of the rest is boilerplate
+assembly = """
+// rax = rdi % rsi
+mov rax, rdi
+div rsi
+mov rax, rdx
+"""
+
+# the hostname will always be the same as challenge binary
+with process(f"/challenge/{os.getenv('HOSTNAME')}") as target:
+    info( target.readrepeat(1) )
+    target.send( asm(assembly) )
+    info( target.readrepeat(1) )
