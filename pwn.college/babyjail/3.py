c4ef463f726a0a33897a59a503efe872bba2d11f
Pratik Devkota <reusedbottle@proton.me>
2022-08-04 18:09:12 +0545

    Update pwn.college/babyjail/3.py
    Removed comment from old chall

--- pwn.college/babyjail/3.py
+++ pwn.college/babyjail/3.py
@@ -20,10 +20,6 @@ assembly += shellcraft.amd64.linux.syscall("SYS_exit", 0);
 
 # the hostname will always be the same as challenge binary
 
-# argv[1] can be anything that doesn't contain 'flag'
-# here, I am just supplying the hostname, then the
-# challenge will start reading shellcode from stdin
-
 challenge_binary = os.getenv('HOSTNAME')
 with process([f"/challenge/{challenge_binary}", "/"]) as target:
     info( target.readrepeat(1) )
